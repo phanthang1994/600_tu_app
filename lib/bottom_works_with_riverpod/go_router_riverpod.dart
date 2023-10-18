c4b0b588e852fac7bb62395253760aa1a2c7d89c
phanx <phanxuanthang20@gmail.com>
2023-10-18 21:22:57 +0700

    xoas thua

--- lib/bottom_works_with_riverpod/go_router_riverpod.dart
+++ lib/bottom_works_with_riverpod/go_router_riverpod.dart
@@ -6,15 +6,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../provider.dart';
 
-/// This sample app shows an app with two screens.
-///
-/// The first route '/' is mapped to [HomeScreen], and the second route
-/// '/details' is mapped to [DetailsScreen].
-///
-/// The buttons use context.go() to navigate to each destination. On mobile
-/// devices, each destination is deep-linkable and on the web, can be navigated
-/// to using the address bar.
-
 void main() => runApp(const ProviderScope(child: MyApp()));
 
 /// The route configuration.
