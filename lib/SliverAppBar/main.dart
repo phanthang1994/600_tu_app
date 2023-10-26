d7a0abe91ee59f5e03e0bb18099f42ec9f7b0130
LeoPhanSotatek <thang.phan2@sotatek.com>
2023-10-26 11:31:11 +0700

    add sliverappbar

---
+++ lib/SliverAppBar/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:tuvungapp600/SliverAppBar/sliverappbar_status.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SliverAppBar Demo',
+      theme: ThemeData(primarySwatch: Colors.teal, brightness: Brightness.dark),
+      home: SliverAppBarStatus(),
+    );
+  }
+}
